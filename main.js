e4474f3e04257eb8b93cf48e591f687d7731647e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-09-24 21:41:08 -0400

    Rivermark Village 1.0  /main.js

--- main.js
+++ main.js
@@ -147,12 +147,6 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                                 name: "Newsletter Sign Up",
                                 href: "/newsletter",
                                 target: null
-                            },
-                            {
-                                name: "Non-Profit Donation Request Form",
-                                href: "//codecloud.cdn.speedyrails.net/sites/5b5e82b96e6f644fb8e40000/application/pdf/1520876234000/The%20Landing%20at%20Renton%20Non-Profit%20Donation%20Form%20(2).pdf",
-                                target: ""
-                                
                             }
                         ]
                     }
