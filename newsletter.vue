5cc062a40604b8746d052fb193975351ce549741
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-09-27 13:55:31 -0400

    Rivermark Village 1.0  /newsletter.vue

---
+++ newsletter.vue
@@ -0,0 +1,160 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ background: 'linear-gradient(0deg, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), #000 url(' + pageBanner.image_url + ') center center' }">
+                    <div class="main_container position_relative">
+                        <h2>Newsletter</h2>
+                    </div>
+                </div>
+                <div class="main_container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <breadcrumb></breadcrumb>
+                            <div v-if="pageContent" v-html="pageContent.body"></div>
+                            
+                            <form id="subForm" class="js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1AE1A7D2AEE821DE838B6B392406C88FEA8BADE94A1924971E494F3165EFCCADF9018AA9C3068C306164CCE896A2785C0C">	
+<p>
+    
+</p>
+<p>
+    
+</p>
+<p>
+    <label for="fieldEmail">Email</label>
+    <br />
+    <input id="fieldEmail" name="cm-yhidpu-yhidpu" type="email" class="js-cm-email-input"
+    required />
+</p>
+<p>
+    <button class="js-cm-submit-button" type="submit">Subscribe</button>
+</p>
+</form>
+<script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
+                            
+                            
+                            
+                            
+                            <form class="newsletter_form form-horizontal" action="//mobilefringe.createsend.com/t/d/s/yhidpu/" method="post">
+                                <div class="row">
+                                    <div class="col-sm-6" >
+                                        <label for="fieldyhljmd" class="accessibility">First Name</label>
+                                        <input v-model="form_data.first_name" required class="margin_20 form-control" id="fieldyhljmd" name="cm-f-yhljmd" type="text" placeholder="First Name" />
+                                    </div>
+                                    <div class="col-sm-6" >
+                                        <label for="fieldyhljmh" class="accessibility">Last Name</label>
+                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldyhljmh" name="cm-f-yhljmh" type="text" placeholder="Last Name" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <label for="newsletter_email" class="accessibility">Email</label>
+                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-qtttjt-qtttjt" type="email" placeholder="Email" id="newsletter_email">
+                                    </div>
+                                    <!--<div class="col-sm-6">-->
+                                    <!--    <label for="fieldglrtdr" class="accessibility">Phone Number</label>-->
+                                    <!--    <input id="fieldglrtdr" v-model="form_data.phone" required class="margin_20 form-control" name="cm-f-glrtdr" type="text" placeholder="Phone Number" />-->
+                                    <!--</div>-->
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div style="margin-left: 20px">
+                                            <label class="checkbox">
+                                                <input name="agree_newsletter" required  type="checkbox">
+                                                    I agree to receive communications from {{ property.name }}.
+                                            </label>
+                                        </div>
+            					    </div>
+            					</div>
+        					    <div class="margin_40 clearfix"></div>
+        					    <div class="row">
+                                    <div class="col-xs-12">
+                                        <button class="animated_btn" type="submit" :disabled="formSuccess">Subscribe</button>
+                                    </div>
+                                </div>
+                                <!--<div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">-->
+                                <!--    <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>-->
+                                <!--    <span class="sr-only">{{$t("newsletter_page.success")}} : </span>-->
+                                <!--    Thank you! Your subscription has been confirmed.-->
+                                <!--</div>-->
+                                <!--<div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">-->
+                                <!--    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>-->
+                                <!--    <span class="sr-only">{{$t("newsletter_page.error")}} : </span>-->
+                                <!--    There was an error when trying to submit your request. Please try again later.-->
+                                <!--</div>-->
+                            </form> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+<script>
+    define(["Vue", "vuex", "jquery", "vee-validate", "json!site.json"], function(Vue, Vuex, $, VeeValidate, site) {
+        Vue.use(VeeValidate);
+        return Vue.component("newsletter-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    dataLoaded: true,
+                    pageContent: null,
+                    siteInfo: site,
+                    form_data : {},
+                    formSuccess : false,
+                    formError: false,
+                    pageBanner: null
+                }
+            },
+            mounted () {
+                this.form_data.first_name = this.$route.query.name;
+                $("#fieldglrtiu").val(this.form_data.first_name);
+                this.form_data.email = this.$route.query.email;
+                $("#newsletter_email").val(this.form_data.email);
+            },
+            watch : {
+                $route () {
+                    this.form_data.first_name = this.$route.query.name;
+                    $("#fieldglrtiu").val(this.form_data.first_name);
+                    this.form_data.email = this.$route.query.email;
+                    $("#newsletter_email").val(this.form_data.email);
+                }
+            },
+            created() {
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Newsletter Banner');
+                    if(temp_repo != null && temp_repo != undefined) {
+                        this.pageBanner = temp_repo.images[0];
+                    } else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5c82cb8f6e6f643f0f010000/image/png/1552582149966/landing_default_banner.png"
+                        }
+                    }
+                    
+                   if(response){
+                        this.pageContent = response[0].data;
+                   }
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'findRepoByName'
+                ])
+            },
+            methods: {
+                loadData: async function () {
+                    this.property.mm_host = this.property.mm_host.replace("http:", "");
+                    try {
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/"+site.subdomain+"-newsletter.json"})]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+            }
+        });
+    });
+</script>
