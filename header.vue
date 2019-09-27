5cce93a48311b972ba93b7ed046235a5a4f533cc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-09-27 13:16:59 -0400

    Rivermark Village 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -34,7 +34,9 @@
     						        <span @click="showDropDown = !showDropDown" v-if="item.sub_menu != undefined">{{ item.name }}</span>
     						        <ul v-show="showDropDown" v-if="item.sub_menu">
     						            <li @click="showDropDown = !showDropDown" v-for="sub_menu in item.sub_menu" class="dropdown_item">
-    						                <router-link :to="sub_menu.href">{{ sub_menu.name }}</router-link>
+    						                <router-link :to="sub_menu.href">
+    						                    <p>{{ sub_menu.name }}</p>
+						                    </router-link>
     						            </li>
     								</ul>
     						    </li>
