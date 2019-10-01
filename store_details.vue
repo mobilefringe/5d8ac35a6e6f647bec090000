1d9b400c1fb1154302c7cae74e54bba319143f2c
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-01 16:14:29 -0400

    Rivermark Village 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -20,7 +20,7 @@
                                 <!--<img class="store_details_image center-block" :src="currentStore.store_front_url_abs" :alt="currentStore.name + ' Logo'" />-->
                                 <div v-if="currentStore.no_logo" class="store_details_no_logo center-block">
                                     <div class="no_logo">
-                                        <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png">
+                                        <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                         <p class="store_details_name">{{ currentStore.name }}</p>
                                     </div>    
                                 </div>
