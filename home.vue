5311f2374a4ead515d736c035b2ee53fe5c13159
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-02 17:03:56 -0400

    Rivermark Village 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -74,7 +74,7 @@
                             <div class="insta-feed-container">
                                 <div class="insta-feed-image " v-for="(item, index) in instaFeed">
                                     <a :href="item.link" target="_blank">
-                                        <div class="insta-img" v-bind:style="{ 'background-image': 'url(' + item.images.standard_resolution.url + ')' }"></div>
+                                        <div class="insta-img" v-bind:style="{ 'background': '#000 url(' + item.images.standard_resolution.url + ')' }"></div>
                                         <div class="insta_content">
                                             <p class="insta_caption">{{ item.caption.text }}</p>
                                             <p class="insta_user">@{{ item.user.username }}</p>
