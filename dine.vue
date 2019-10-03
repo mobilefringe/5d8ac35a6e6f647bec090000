4bb11f8a1e00a6db4761b38b7e44426eb8d38513
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-03 11:13:23 -0400

    Rivermark Village 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -57,7 +57,7 @@
                                 			<!--<img class="store_img" :src="store.image_url" alt="">-->
                                 			<div v-if="!store.no_store_logo">
                                 			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="transparent">
-                                			    <img class="store_img" :src="store.store_front_url_abs" :alt="store.name">
+                                			    <img class="store_img" :src="store.store_front_url_abs" alt="">
                                 			</div>
                                 			
                                             <div v-else class="no_logo_container">
