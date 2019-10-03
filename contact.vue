69a0091e6a97e5ef535bdac2cf4e4ac04e90168c
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-03 12:10:30 -0400

    Rivermark Village 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -145,7 +145,7 @@
                             this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + Site.subdomain + "-contact-us.json" }),
                             this.$store.dispatch("getData", "repos")
                         ]);
-                        console.log(results)
+
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
