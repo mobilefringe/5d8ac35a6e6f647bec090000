2c8fcd5ba9ce8dd3479e859a5d1b60a0d22146c7
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-08 17:03:56 -0400

    Rivermark Village 1.0  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -124,7 +124,7 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Delivery Banner');
+                    var temp_repo = this.findRepoByName('Dining Banner');
                     if (temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
