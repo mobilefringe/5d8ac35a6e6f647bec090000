fbce37f0da761197d458873b20b0622d3df16820
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-09 00:42:51 -0400

    Rivermark Village 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -25,8 +25,8 @@
                                     </div>    
                                 </div>
                                 <div id="store_dets_logo_container" v-else>
-                                    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="Current Store">
-                    			    <img  class="store_details_image" :src="currentStore.store_front_url_abs" alt="Current Store">
+                                    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
+                    			    <img  class="store_details_image" :src="currentStore.store_front_url_abs" alt="">
                                 </div>
                                 <div class="margin_20 center" v-if="currentStore.phone">
                                     <a class="store_details_phone" :href="'tel:' + currentStore.phone">{{ currentStore.phone }}</a>    
