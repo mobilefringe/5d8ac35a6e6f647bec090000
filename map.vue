1dea4e720ab799931cf98868c93e69ea303156ae
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-21 12:48:22 -0400

    Rivermark Village 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -109,7 +109,7 @@
                     return this.processedCategories;
                 },
                 dropDownCats() {
-                    var cats = _.filter(this.processedCategories, function(o) { return o.name != "Dine Filter" && o.store_ids != null; });
+                    var cats = _.filter(this.processedCategories, function(o) { return o.name != "Dine Filter" });
                     cats = _.map(cats, 'name');
                     cats.unshift('All');
                     return cats;
