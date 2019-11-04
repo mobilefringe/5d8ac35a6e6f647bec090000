d72cd1d08190c4c50d11613719aecc2a6d3cac17
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-04 13:39:13 -0500

    Rivermark Village 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -189,7 +189,6 @@
                        subcategories = vm.findSubcategoriesByParentID(dining_cat.id);
                     }
                     subcategories = _.map(subcategories, 'name').sort();
-                    console.log(dining_cat, subcategories)
                     subcategories.unshift('All Cuisine');
                     return subcategories;
                 },
