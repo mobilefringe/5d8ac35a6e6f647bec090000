79d9cf635501ab99aaea03c48234641f263d70ba
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-12-02 19:52:12 -0500

    Rivermark Village 1,1  /stores.vue

--- stores.vue
+++ stores.vue
@@ -52,7 +52,7 @@
                             <transition-group name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" tag="div">
                                 <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
                             	    <div class="store_logo_container">
-                            	        <router-link :to="'/stores/'+ store.slug">
+                            	        <router-link :to="'/stores/'+ store.slug" :aria-label="'Go to store details for ' + store.name">
                                 			<!--<img class="store_img" :src="store.image_url" alt="">-->
                                 			<div v-if="!store.no_store_logo">
                                 			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="default logo">
