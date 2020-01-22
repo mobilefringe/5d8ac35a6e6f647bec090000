88b55604c14484e3927904b6f9c3995a09301e42
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2020-01-22 11:13:49 -0500

    Rivermark Village 1,1  /contest.vue

--- contest.vue
+++ contest.vue
@@ -107,7 +107,7 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Contest Banner');
+                    var temp_repo = this.findRepoByName('Events Banner');
                     if(temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
